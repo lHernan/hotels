18ec43d2874dc91ea53349adc348c3d542ede44c
lHernan <hernan.laguado@gmail.com>
2020-01-10 12:44:40 -0500

    Update HotelDetail.js

--- react-api/src/components/HotelDetail.js
+++ react-api/src/components/HotelDetail.js
@@ -4,7 +4,8 @@ class HotelDetail extends Component {
 
   state = {
     details: {},
-    locationInfo: {}
+    locationInfo: {},
+    latlng:{},
   }
 
   componentDidMount() { 
@@ -12,14 +13,12 @@ class HotelDetail extends Component {
     var url = window.location.href.split('/');
     var hotelId = url[url.length-1];
 
-    console.log('Hotel id: '+ hotelId);
-
     fetch('http://localhost:5000/getHotelById/'+hotelId)
     .then(res => res.json())
     .then((data) => {
       this.setState({ details: data })
-      this.setState({ locationInfo: data.location })
-      console.log(data);
+      this.setState({ locationInfo: data.location })      
+      this.setState({ latlng: data.location.geo })  
     })
     .catch(console.log)
   }
@@ -30,7 +29,13 @@ class HotelDetail extends Component {
       <br></br>
       <h2>{this.state.details.name}</h2>
       <h4>{this.state.details.mail}</h4>
-      <h4>{this.state.locationInfo.address}</h4>
+      <h4>{this.state.locationInfo.address}, {this.state.locationInfo.city}, {this.state.locationInfo.zipcode}</h4>
+      <h4>{this.state.latlng.lat}</h4>
+      <h4>{this.state.latlng.lng}</h4>
+      <h4>{this.state.details.currency}</h4>
+      <h4>{this.state.details.web}</h4>
+      <img src={this.state.details.image} alt={this.state.details.name} /> 
+      
     </div> 
   );
   }
