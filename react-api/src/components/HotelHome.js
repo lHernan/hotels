c385ebcf5a43016ac9e9d2b9e442fab15068248f
lHernan <hernan.laguado@gmail.com>
2020-01-09 22:09:29 -0500

    Update HotelHome.js

--- react-api/src/components/HotelHome.js
+++ react-api/src/components/HotelHome.js
@@ -1,11 +1,23 @@
-import React from "react";
+import React, { Component }from "react";
+import Hotels from './Hotels.js';
 
-function HotelHome(props) {
-  return (
-    <div>
-      HotelHome
-     </div> 
-  ); 
-} 
+class HotelHome extends Component {
+  state = {
+    hotels: []
+  }
+  componentDidMount() {
+    fetch('http://localhost:5000/getAllHotels')
+    .then(res => res.json())
+    .then((data) => {
+      this.setState({ hotels: data })
+    })
+    .catch(console.log)
+  }
+  render() {
+    return (
+      <Hotels hotels={this.state.hotels} />
+    );
+  }
+}
 
 export default HotelHome;
